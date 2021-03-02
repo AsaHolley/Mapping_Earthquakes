486987b3ba2c4d92ba2ec2792ea91565c45da882
AsaHolley <asa.holley32@gmail.com>
2021-03-01 20:03:11 -0500

    module work

--- Mapping_GeoJSON_Lines/static/js/logic.js
+++ Mapping_GeoJSON_Lines/static/js/logic.js
@@ -2,8 +2,8 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+let light = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,8 +12,8 @@ let streets = L.tileLayer(
   }
 );
 // We create the dark view tile layer that will be an option for our map.
-let satelliteStreets = L.tileLayer(
-  "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+let dark = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -24,15 +24,15 @@ let satelliteStreets = L.tileLayer(
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-  Streets: streets,
-  "Satellite Streets": satelliteStreets,
+  Street: light,
+  Dark: dark,
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map("mapid", {
-  center: [43.7, -79.3],
-  zoom: 11,
-  layers: [satelliteStreets],
+  center: [44.0, -80.0],
+  zoom: 2,
+  layers: [light],
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
